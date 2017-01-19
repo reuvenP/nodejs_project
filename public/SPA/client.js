640bd44ae5995bd3eace5c5a17815b1e35955630
Ariel Ben-Ami <aba4000@gmail.com>
2017-01-19 03:22:50 +0200

    users management continued

--- public/SPA/client.js
+++ public/SPA/client.js
@@ -1,20 +1,20 @@
 var app = angular.module('myapp', ['ngAnimate', 'ui.bootstrap', 'ngRoute']);
 
-app.factory('usersService', ['$http', '$q', function($http, $q) {
+app.factory('usersService', ['$http', '$q', function ($http, $q) {
     var usersList = [];
-    var getUsers = function() {
+    var getUsers = function () {
         var deferred = $q.defer();
-        $http.get('/users/getUsers').then(function(response) {
+        $http.get('/users/getUsers').then(function (response) {
             usersList = response.data;
             deferred.resolve(usersList);
-        }, function(response) {
+        }, function (response) {
             deferred.reject(response);
         });
 
         return deferred.promise;
     };
 
-    var deleteUser = function(userId) {
+    var deleteUser = function (userId) {
         var deferred = $q.defer();
         $http.delete('/users/deleteUser/' + userId)
             .then(function (response) {
@@ -22,7 +22,7 @@ app.factory('usersService', ['$http', '$q', function($http, $q) {
                 deferred.resolve(usersList);
             }, function (response) {
                 deferred.reject(response);
-        });
+            });
 
         return deferred.promise;
     };
@@ -31,6 +31,119 @@ app.factory('usersService', ['$http', '$q', function($http, $q) {
     usersService.getUsers = getUsers;
     usersService.deleteUser = deleteUser;
     return usersService;
+}]);
+
+app.factory('userDetailsInit', [function () {
+    var init = function () {
+        $('#contact_form').bootstrapValidator({
+            // To use feedback icons, ensure that you use Bootstrap v3.1.0 or later
+            feedbackIcons: {
+                valid: 'glyphicon glyphicon-ok',
+                invalid: 'glyphicon glyphicon-remove',
+                validating: 'glyphicon glyphicon-refresh'
+            },
+            fields: {
+                first_name: {
+                    validators: {
+                        stringLength: {
+                            min: 2,
+                        },
+                        notEmpty: {
+                            message: 'Please supply your first name'
+                        }
+                    }
+                },
+                last_name: {
+                    validators: {
+                        stringLength: {
+                            min: 2,
+                        },
+                        notEmpty: {
+                            message: 'Please supply your last name'
+                        }
+                    }
+                },
+                email: {
+                    validators: {
+                        notEmpty: {
+                            message: 'Please supply your email address'
+                        },
+                        emailAddress: {
+                            message: 'Please supply a valid email address'
+                        }
+                    }
+                },
+                phone: {
+                    validators: {
+                        notEmpty: {
+                            message: 'Please supply your phone number'
+                        },
+                        phone: {
+                            country: 'US',
+                            message: 'Please supply a vaild phone number with area code'
+                        }
+                    }
+                },
+                address: {
+                    validators: {
+                        stringLength: {
+                            min: 8,
+                        },
+                        notEmpty: {
+                            message: 'Please supply your street address'
+                        }
+                    }
+                },
+                state: {
+                    validators: {
+                        stringLength: {
+                            min: 4,
+                        },
+                        notEmpty: {
+                            message: 'Please supply your city'
+                        }
+                    }
+                },
+                city: {
+                    validators: {
+                        notEmpty: {
+                            message: 'Please select your state'
+                        }
+                    }
+                },
+                comment: {
+                    validators: {
+                        stringLength: {
+                            min: 10,
+                            max: 200,
+                            message: 'Please enter at least 10 characters and no more than 200'
+                        },
+                        notEmpty: {
+                            message: 'Please supply a description of your project'
+                        }
+                    }
+                }
+            }
+        }).on('success.form.bv', function (e) {
+            $('#success_message').slideDown({opacity: "show"}, "slow") // Do something ...
+            $('#contact_form').data('bootstrapValidator').resetForm();
+
+            // Prevent form submission
+            e.preventDefault();
+
+            // Get the form instance
+            var $form = $(e.target);
+
+            // Get the BootstrapValidator instance
+            var bv = $form.data('bootstrapValidator');
+
+            // Use Ajax to submit form data
+            $.post($form.attr('action'), $form.serialize(), function (result) {
+                console.log(result);
+            }, 'json');
+        });
+    };
+    return init;
 }]);
 
 app.config(function ($routeProvider) {
@@ -54,42 +167,26 @@ app.config(function ($routeProvider) {
         });
 });
 
-app.controller('userDetailsCtrl', ['user', '$uibModalInstance', 'returnUrl', userDetailsCtrl]);
-function userDetailsCtrl(user, $uibModalInstance, returnUrl) {
+app.controller('userDetailsCtrl', ['$uibModalInstance', 'userDetailsInit', 'user', 'returnUrl', userDetailsCtrl]);
+function userDetailsCtrl($uibModalInstance, userDetailsInit, user, returnUrl) {
     var ctrl = this;
     ctrl.user = user;
 
-    ctrl.ok = function() {
-        $uibModalInstance.close();
-        window.location = returnUrl;
-    };
-
-    ctrl.cancel = function() {
-        $uibModalInstance.dismiss();
-        window.location = returnUrl;
-    };
-}
-
-function openModalController($uibModal, model, controller, modelParamName, templateUrl, returnUrl) {
-    var resolve = {};
-    resolve[modelParamName] = function() {
-        return model;
-    };
-    resolve['returnUrl'] = function() {
-        return returnUrl;
-    };
-
-    $uibModal.open({
-        animation: true,
-        backdrop: 'static',
-        windowsClass: 'center-modal',
-        size: 'sm',
-        templateUrl: templateUrl,
-        controller: controller,
-        keyboard: false,
-        controllerAs: 'ctrl',
-        resolve: resolve
+    $uibModalInstance.rendered.then(function () {
+        userDetailsInit();
     });
+
+    $uibModalInstance.closed.then(function () {
+        window.location = returnUrl;
+    });
+
+    ctrl.ok = function () {
+        $uibModalInstance.close();
+    };
+
+    ctrl.cancel = function () {
+        $uibModalInstance.dismiss();
+    };
 }
 
 app.controller('usersController', ['$routeParams', '$uibModal', 'usersService', usersController]);
@@ -100,9 +197,25 @@ function usersController($routeParams, $uibModal, usersService) {
         function (users) {
             ctrl.usersList = users;
             if ($routeParams.operation && $routeParams.username) {
-                for (var i=0; i < ctrl.usersList.length; i++) {
+                for (var i = 0; i < ctrl.usersList.length; i++) {
                     if (ctrl.usersList[i].username === $routeParams.username) {
-                        openModalController($uibModal, ctrl.usersList[i], userDetailsCtrl, 'user', 'editUserDetails.html', '/#/users');
+                        $uibModal.open({
+                            animation: true,
+                            backdrop: 'static',
+                            windowsClass: 'center-modal',
+                            size: 'md',
+                            templateUrl: 'SPA/views/editUser.html',
+                            controller: userDetailsCtrl,
+                            controllerAs: 'ctrl',
+                            resolve: {
+                                user: function () {
+                                    return ctrl.usersList[i];
+                                },
+                                returnUrl: function () {
+                                    return '/#/users';
+                                }
+                            }
+                        });
                         break;
                     }
                 }
@@ -119,10 +232,10 @@ function usersController($routeParams, $uibModal, usersService) {
                 ctrl.usersList = users;
             }, function (response) {
                 ctrl.error = response.status + ' - ' + response.statusText + ": " + response.data;
-         });
+            });
     };
 
-    ctrl.editUser = function(user) {
+    ctrl.editUser = function (user) {
         window.location = "/#/users/edit/" + user.username;
     };
 }
