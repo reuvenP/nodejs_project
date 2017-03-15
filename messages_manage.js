afed1ae3990319001e4b0f98f5199c1ef9487312
reuvenp <reuvenp@checkpoint.com>
2017-03-15 09:07:22 +0200

    messages

--- messages_manage.js
+++ messages_manage.js
@@ -1,3 +1,20 @@
 /**
  * Created by Home on 15/03/2017.
  */
+var Message = require('./models/message');
+
+function addMessage(sender, room, text, link, img, isOnlyForConnected) {
+    var message = new Message({
+        sender: sender,
+        room: room,
+        text: text,
+        link: link,
+        img: img,
+        isOnlyForConnected: isOnlyForConnected
+    });
+    message.save(function (err) {
+        if (err) throw err;
+    });
+}
+
+addMessage('me', 'r1', 'bla', 'http://bla.com', undefined, false);
